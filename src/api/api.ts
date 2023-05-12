9fc5c6113b775af4f6d945f1d7182d6c935729a8
Sultan <sitexpressorg@gmail.com>
2023-05-12 12:35:36 +0800

    url env fixed

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:3333/',
+    // baseURL: 'http://localhost:3333/',
+    baseURL: 'REACT_APP_FEEDER_URL',
 })
 
